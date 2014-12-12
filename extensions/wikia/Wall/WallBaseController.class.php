edcf1baa98f38cf1f560704c196ba493d9e45976
aquilax <evgeniy@wikia-inc.com>
2014-12-12 08:25:30 +0000

    Global block overwrites local allowence

--- extensions/wikia/Wall/WallBaseController.class.php
+++ extensions/wikia/Wall/WallBaseController.class.php
@@ -614,7 +614,9 @@ class WallBaseController extends WikiaController{
 	protected function checkAndSetUserBlockedStatus($wallOwner = null) {
 		$user = $this->app->wg->User;
 
-		if( $user->isBlocked() || $user->isBlockedGlobally() ) {
+		if ( $user->isBlockedGlobally() ) {
+			$this->response->setVal('userBlocked', true);
+		} else if ( $user->isBlocked() ) {
 			if(	!empty($wallOwner) &&
 				$wallOwner->getName() == $this->wg->User->getName() &&
 				!(empty($user->mAllowUsertalk)) ) {
@@ -627,7 +629,6 @@ class WallBaseController extends WikiaController{
 		} else {
 			$this->response->setVal('userBlocked', false);
 		}
-
 	}
 
 	public function getThread($filterid) {
